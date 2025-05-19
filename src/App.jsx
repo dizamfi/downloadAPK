01bba21e8b4e43ca965fd5142e3ad95677cad9e8
Diego Zambrano Figueroa <69157845+dizamfi@users.noreply.github.com>
2025-05-19 11:10:14 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
 
     // URL del backend donde está el archivo
     const API_URL = import.meta.env.VITE_API_URL || 'https://apkbackend.onrender.com';
-    const downloadUrl = `${API_URL}/download/1747628237371-base.apk`;
+    const downloadUrl = `${API_URL}/download/1747670791202-base.apk`;
 
     // Crear y disparar un enlace para descargar
     const a = document.createElement('a');
